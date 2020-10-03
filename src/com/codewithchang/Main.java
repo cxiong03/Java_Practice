104b5f5896b673b61433bb5f3bd3157ea547458b
Chang <cnxiong03@gmail.com>
2020-10-03 14:50:04 -0400

    created while loop to keep running until user type quit

--- src/com/codewithchang/Main.java
+++ src/com/codewithchang/Main.java
@@ -1,9 +1,18 @@
 package com.codewithchang;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-        for(int i = 5; i > 0; i--)
-            System.out.println("Hello Chang " + i);
+        Scanner scanner = new Scanner(System.in);
+        String input = "";
+        while(!input.equals("quit")) {
+            System.out.print("Input: ");
+            input = scanner.next().toLowerCase();
+            System.out.println(input);
+        }
     }
 }
+
+// While loop are useful for when we Do Not know how many times we want to repeat something.
