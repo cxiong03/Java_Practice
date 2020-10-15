207c32c8ef0a9989061130093d98665cd53daede
Chang <cnxiong03@gmail.com>
2020-10-14 23:01:14 -0400

    creating method

--- src/com/codewithchang/Main.java
+++ src/com/codewithchang/Main.java
@@ -3,15 +3,11 @@ package com.codewithchang;
 public class Main {
 
     public static void main(String[] args) {
-        String[] fruits = { "Apple", "Strawberry", "Orange" };
-
-        // Using the forloop we can iterate from the end to the beginning
-        for (int i = fruits.length; i > 0; i--)
-            System.out.println(fruits[i]);
-
-        // ForEach loops are used to iterate through arrays or collections but its limitation is it's always forward only
-        for (String fruit : fruits)
-            System.out.println(fruit);
+        greetUser("Chang", "Xiong");
     }
+
+        public static void greetUser(String firstName, String lastName) {
+            System.out.println("Hello " + firstName + " " + lastName);
+        }
 }
 
