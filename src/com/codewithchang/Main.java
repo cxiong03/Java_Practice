6b80daab1e1b9940abe979854700a822bcc4fb2d
Chang <cnxiong03@gmail.com>
2020-10-18 23:36:24 -0400

    TextBox class created

--- src/com/codewithchang/Main.java
+++ src/com/codewithchang/Main.java
@@ -3,12 +3,7 @@ package com.codewithchang;
 public class Main {
 
     public static void main(String[] args) {
-        String message = greetUser("Chang", "Xiong");
-        System.out.println(message);
-    }
 
-        public static String greetUser(String firstName, String lastName) {
-            return "Hello " + firstName + " " + lastName;
-        }
+    }
 }
 
