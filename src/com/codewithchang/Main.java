a6b353f9a2fb48bdac7d03934ee449b1dcee5039
Chang <cnxiong03@gmail.com>
2020-10-25 22:58:19 -0400

    private methods in Browser class to reduce unnecessary details

--- src/com/codewithchang/Main.java
+++ src/com/codewithchang/Main.java
@@ -3,11 +3,7 @@ package com.codewithchang;
 public class Main {
 
     public static void main(String[] args) throws IllegalAccessException {
-        Employee newEmployee = new Employee();
-        newEmployee.setBaseSalary(50_000);
-        newEmployee.setHourlyRate(20);
-        int wage = newEmployee.calculateWage(10);
-        System.out.println(wage);
+        Browser browser = new Browser();
     }
 }
 
