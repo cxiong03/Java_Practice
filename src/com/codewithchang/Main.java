37019a60f56547d9dc1b6437aa4070f0be780163
Chang <cnxiong03@gmail.com>
2020-11-08 15:06:28 -0500

    constructors and inheritance

--- src/com/codewithchang/Main.java
+++ src/com/codewithchang/Main.java
@@ -3,11 +3,7 @@ package com.codewithchang;
 public class Main {
 
     public static void main(String[] args) {
-        TextBox box1 = new TextBox();
-        TextBox box2 = box1;
-        System.out.println(box1.hashCode());
-        System.out.println(box2.hashCode());
-        System.out.println(box1.equals(box2)); // Hash code are use to compare object for equality
+        TextBox textBox = new TextBox();
     }
 }
 
