0edeab35acb3f4191192c35433e3c76f92c37a35
Chang <cnxiong03@gmail.com>
2020-11-08 16:52:23 -0500

    upcasting and downcasting

--- src/com/codewithchang/Main.java
+++ src/com/codewithchang/Main.java
@@ -3,9 +3,17 @@ package com.codewithchang;
 public class Main {
 
     public static void main(String[] args) {
+        UIControl control = new UIControl(true);
         TextBox textBox = new TextBox();
-        textBox.setText("Hello Chang");
-        System.out.println(textBox);
+        show(textBox);
+    }
+
+    public static void show(UIControl control) {
+        if (control instanceof TextBox) {
+            TextBox textBox = (TextBox) control; // Downcasting
+            textBox.setText("Hello Chang");
+        }
+        System.out.println(control);
     }
 }
 
