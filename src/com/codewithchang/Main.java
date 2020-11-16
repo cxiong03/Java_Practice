85d911b0ee7e3658e9c177134217596dfe426527
Chang <cnxiong03@gmail.com>
2020-11-15 23:47:35 -0500

    constructor injection

--- src/com/codewithchang/Main.java
+++ src/com/codewithchang/Main.java
@@ -3,7 +3,8 @@ package com.codewithchang;
 public class Main {
 
     public static void main(String[] args) {
-
+        TaxCalculator2020 calculator = new TaxCalculator2020(100_000);
+        TaxReport report = new TaxReport(calculator);
     }
 }
 
